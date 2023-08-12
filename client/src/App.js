578fb8367c804fc30db76ef391cbb604894257bf
jash413 <jashmistry4444@gmail.com>
2023-08-12 15:31:05 +0530

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 // import Dashboard from "./pages/Dashboard";
 // import PatientForm from "./pages/Add-patient";
 import Patientlist from "./pages/PatientList";
->>>>>>> f801806a85a4271ed6870c923a8c28e3bd6d79fb
 function App() {
   return (
     <div>
-      {/* <PatientForm/> */}
-<<<<<<< HEAD
-      <UploadDocument/>
-=======
+
       <Patientlist/>
->>>>>>> f801806a85a4271ed6870c923a8c28e3bd6d79fb
     </div>
   );
 }

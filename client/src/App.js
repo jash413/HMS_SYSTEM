760415df6ca490313cb1028d9586e570d315c804
jash413 <jashmistry4444@gmail.com>
2023-08-15 01:02:27 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@
 // import PatientForm from "./pages/Add-patient";
 // import Patientlist from "./pages/PatientList";
 // import Labs from "./pages/Labs";
-// import Patientlist from "./pages/PatientList";
-import Labs from "./pages/Labs";
 // import UploadDocument from "./pages/Upload-document";
 function App() {
   return (

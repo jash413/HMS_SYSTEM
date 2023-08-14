5ead05e65bbe1eb28f238b8ee505d54adf8f7136
jash413 <jashmistry4444@gmail.com>
2023-08-15 00:48:36 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
 // import Dashboard from "./pages/Dashboard";
 // import PatientForm from "./pages/Add-patient";
-<<<<<<< HEAD
 // import Patientlist from "./pages/PatientList";
 import Labs from "./pages/Labs";
-=======
 import Patientlist from "./pages/PatientList";
->>>>>>> 7dbb82817aa4bbf1fdf1f67d817041e8bd42bbe1
 // import UploadDocument from "./pages/Upload-document";
 function App() {
   return (
     <div>
       {/* <UploadDocument /> */}
-<<<<<<< HEAD
       <Labs/>
-=======
       {/* <PatientForm /> */}
       <Patientlist />
->>>>>>> 7dbb82817aa4bbf1fdf1f67d817041e8bd42bbe1
     </div>
   );
 }

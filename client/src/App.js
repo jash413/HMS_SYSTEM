06cdb0d52bd3d1005e344d7812bd5bb0a60415f0
Majithia Devansh <123489552+mdevansh3012@users.noreply.github.com>
2023-08-15 00:45:56 +0530

    dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,13 @@
 // import Dashboard from "./pages/Dashboard";
 // import PatientForm from "./pages/Add-patient";
-<<<<<<< HEAD
-import Patientlist from "./pages/PatientList";
-import UploadDocument from "./pages/Upload-document";
-function App() {
-  return (
-    <div>
-      <Patientlist/>
-=======
-<<<<<<< HEAD
 // import Patientlist from "./pages/PatientList";
 import Labs from "./pages/Labs";
-=======
-import Patientlist from "./pages/PatientList";
->>>>>>> 7dbb82817aa4bbf1fdf1f67d817041e8bd42bbe1
 // import UploadDocument from "./pages/Upload-document";
 function App() {
   return (
     <div>
       {/* <UploadDocument /> */}
-<<<<<<< HEAD
       <Labs/>
-=======
-      {/* <PatientForm /> */}
-      <Patientlist />
->>>>>>> 7dbb82817aa4bbf1fdf1f67d817041e8bd42bbe1
->>>>>>> 750fe038d7534fc6fe5a1aa8b4ee3b16c5969beb
     </div>
   );
 }

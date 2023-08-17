56321ac99ee50e023ea5ed95d2021527f6be7778
Majithia Devansh <123489552+mdevansh3012@users.noreply.github.com>
2023-08-17 15:44:20 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 // import Dashboard from "./pages/Dashboard";
 // import PatientForm from "./pages/Add-patient";
 // import Patientlist from "./pages/PatientList";
-// import Labs from "./pages/Labs";
+import Labs from "./pages/Labs";
 // import UploadDocument from "./pages/Upload-document";
 function App() {
   return (
     <div>
       {/* <UploadDocument />1 */}
-      {/* <Labs/> */}
+      <Labs/>
       {/* <PatientForm /> */}
       {/* <Patientlist /> */}
     </div>

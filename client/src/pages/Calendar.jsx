93833f72b6fc872050288d785abb342624c24ab1
Majithia Devansh <123489552+mdevansh3012@users.noreply.github.com>
2023-09-01 00:29:30 +0530

    changes

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import moment from "moment";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import moment from 'moment';
+import axios from 'axios';
+
 const renderEventContent = (eventInfo) => {
   const isSurgery = eventInfo.event.classNames.includes('surgery-event');
   const eventStyle = {
@@ -29,21 +29,39 @@ const renderEventContent = (eventInfo) => {
   );
 };
 
-// import Modal from 'react-modal';
 function Calendar() {
   const [appointments, setAppointments] = useState([]);
-
+  const [appointmentsFromSurgerySection, setAppointmentsFromSurgerySection] = useState([]);
   useEffect(() => {
-    // Fetch appointment data from API
-    axios
-      .get("http://localhost:3100/api/ap") // Replace with your API endpoint
-      .then((response) => {
+    // Fetch appointments data from the "Appointment" form API
+    axios.get('http://localhost:3100/api/ap') // Replace with the actual endpoint
+      .then(response => {
         setAppointments(response.data);
       })
-      .catch((error) => {
-        console.error("Error fetching appointments:", error);
+      .catch(error => {
+        console.error('Error fetching appointments from "Appointment" form:', error);
+      });
+
+    // Fetch surgery data from the "Surgery" API
+    axios.get('http://localhost:3100/surgeries') // Replace with the actual endpoint
+    .then(response => {
+      // Map surgery appointments
+      const surgeryAppointments = response.data.map(surgery => ({
+        ...surgery,
+        title: 'Surgery',
+        admissionDate: surgery.start_time, // Use start_time as admissionDate
+        admissionTime: moment(surgery.start_time).format('HH:mm'), // Extract time from start_time
+        to: moment(surgery.end_time).format('HH:mm'), // Extract time from end_time
+      }));
+      setAppointmentsFromSurgerySection(surgeryAppointments);
+    })
+      .catch(error => {
+        console.error('Error fetching surgeries from "Surgery" API:', error);
       });
   }, []);
+
+  const allAppointments = [...appointments, ...appointmentsFromSurgerySection];
+  console.log(allAppointments)
 
   return (
     <div className="body d-flex py-lg-3 py-md-2">
@@ -52,31 +70,23 @@ function Calendar() {
           <div className="col-lg-12 col-md-12">
             <div className="card">
               <div className="card-body">
-                {/* <button
-                className="btn btn-primary"
-                onClick={() => setIsModalOpen(true)}
-              >
-                Add Event
-              </button> */}
                 <FullCalendar
-  plugins={[dayGridPlugin]}
-  initialView="dayGridMonth"
-  weekends={true}
-  events={appointments.map(appointment => ({
-    title: `${appointment.title} (${appointment.admissionTime} - ${appointment.to})`,
-    start: moment(appointment.admissionDate + ' ' + appointment.admissionTime, 'YYYY-MM-DD hh:mm ').toDate(),
-    end: moment(appointment.admissionDate + ' ' + appointment.endTime, 'YYYY-MM-DD hh:mm ').toDate(),
-    classNames: [appointment.title === 'Surgery' ? 'surgery-event' : 'normal-event'],
-  }))}
-  eventContent={renderEventContent}
-/>
-
+                  plugins={[dayGridPlugin]}
+                  initialView="dayGridMonth"
+                  weekends={true}
+                  events={allAppointments.map(appointment => ({
+                    title: `${appointment.title} (${appointment.admissionTime} - ${appointment.to})`,
+                    start: moment(appointment.admissionDate + ' ' + appointment.admissionTime, 'YYYY-MM-DD hh:mm ').toDate(),
+                    end: moment(appointment.admissionDate + ' ' + appointment.endTime, 'YYYY-MM-DD hh:mm ').toDate(),
+                    classNames: [appointment.title === 'Surgery' ? 'surgery-event' : 'normal-event'],
+                  }))}
+                  eventContent={renderEventContent}
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* React Modal */}
     </div>
   );
 }

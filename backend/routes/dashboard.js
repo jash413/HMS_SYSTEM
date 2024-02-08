3c22d1fe821167afbd90159d82ea5a1ca81c3010
jash413 <jashmistry4444@gmail.com>
2024-02-08 11:22:10 +0530

    delete

--- backend/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-
-// Get hospitality status
-router.post('/dashboard/hospitalityStatus', dashboardController.getHospitalityStatus);
-
-module.exports = router;
--- backend/routes/pdf.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pdfController = require('../controllers/pdfController'); // Adjust the path as needed
-
-// Route for PDF extraction
-router.post('/api/extract', pdfController.extractPdfData);
-
-module.exports = router;
